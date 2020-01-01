8f8fd5b200a0169359d20d0278fb5c0b06a5b336
dancoe77 <dan@shoeman-tech.com>
2019-12-31 17:47:27 -0800

    fifth commit

--- 017-Loop/02-nesting_loops/main.go
+++ 017-Loop/02-nesting_loops/main.go
@@ -5,8 +5,9 @@ import "fmt"
 func main() {
 	// for init; condition; post {}
 	for i := 0; i <= 100; i++ {
+		fmt.Printf("The outer loop: %d\n", i)
 		for j := 0; j < 3; j++ {
-			fmt.Printf("The outer loop: %d\t The inner loop: %d\n", i, j)
+			fmt.Printf("\t\t The inner loop: %d\n", j)
 
 		}
 

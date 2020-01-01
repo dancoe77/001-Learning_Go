35332fad21665fc2fe62f17afe7b0696b4bc63da
dancoe77 <dan@shoeman-tech.com>
2019-12-31 20:02:31 -0800

    commit twenty-five

--- 008-Control_Flow/02-Conditional/01-if_statement/main.go
+++ 008-Control_Flow/02-Conditional/01-if_statement/main.go
@@ -18,4 +18,20 @@ func main() {
 	if !false {
 		fmt.Println("004")
 	}
+
+	if 2 == 2 {
+		fmt.Println("005")
+	}
+
+	if 2 != 2 {
+		fmt.Println("006")
+	}
+
+	if !(2 == 2) {
+		fmt.Println("007")
+	}
+
+	if !(2 != 2) {
+		fmt.Println("008")
+	}
 }

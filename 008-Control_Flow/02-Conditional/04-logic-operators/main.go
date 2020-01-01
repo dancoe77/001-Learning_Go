bd749bc499ae75737e25e15d65728bd5ab92488c
dancoe77 <dan@shoeman-tech.com>
2019-12-31 21:52:09 -0800

    commit forty-four

--- 008-Control_Flow/02-Conditional/04-logic-operators/main.go
+++ 008-Control_Flow/02-Conditional/04-logic-operators/main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(true && true)
-	fmt.Println(true && false)
-	fmt.Println(true || true)
-	fmt.Println(true || false)
-	fmt.Println(!true)
+	fmt.Printf("true && true\t %v\n", true && true)
+	fmt.Printf("true && false\t %v\n", true && false)
+	fmt.Printf("true || true\t %v\n", true || true)
+	fmt.Printf("true || false\t %v\n", true || false)
+	fmt.Printf("!true\t %v\n", !true)
+	fmt.Printf("!false\t %v\n", !false)
 }

c1ec08e9a0d53029362089bfa1076770618abffd
dancoe77 <dan@shoeman-tech.com>
2019-12-31 19:03:08 -0800

    fifteenth commit

--- 008-Control_Flow/01-Loop/04-break_continue/main.go
+++ 008-Control_Flow/01-Loop/04-break_continue/main.go
@@ -6,4 +6,17 @@ func main() {
 	x := 83 / 40
 	y := 83 % 40
 	fmt.Println(x, y)
+
+	x = 0
+	for {
+		x++
+		if x > 100 {
+			break
+		}
+		if x%2 != 0 {
+			continue
+		}
+		fmt.Println(x)
+	}
+	fmt.Println("done.")
 }

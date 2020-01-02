aa0b8c362c09875264fc0329e93a235fa83fb95d
dancoe77 <dan@shoeman-tech.com>
2020-01-01 18:42:29 -0800

    commit sixty

---
+++ 009-Jedi-III/09-jedi-hands_on/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	favSport := "hockey"
+	switch favSport {
+	case "basketball":
+		fmt.Println("basketball")
+	case "baseball":
+		fmt.Println("baseball")
+	case "football":
+		fmt.Println("football")
+	case "wrestling":
+		fmt.Println("wrestling")
+	case "hockey":
+		fmt.Println("hockey")
+	}
+}

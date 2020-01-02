e18c73db88689e6cd877051573b72b6147867700
dancoe77 <dan@shoeman-tech.com>
2020-01-01 16:10:45 -0800

    commit forty-nine

--- 009-Jedi-III/02-jedi-hands_on/main.go
+++ 009-Jedi-III/02-jedi-hands_on/main.go
@@ -5,8 +5,6 @@ import "fmt"
 func main() {
 	for i := 65; i <= 90; i++ {
 		fmt.Printf("%d\n", i)
-		for j := 0; j < 3; j++ {
-			fmt.Printf("\t\t%#U\n", j)
-		}
+		fmt.Printf("\t%#U\n\t%#U\n\t%#U\n", i, i, i)
 	}
 }

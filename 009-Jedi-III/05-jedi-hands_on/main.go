bb95b8bf2880d15522e19a1b1ad7f0e69fa99b21
dancoe77 <dan@shoeman-tech.com>
2020-01-01 17:12:23 -0800

    commit fifty-six

--- 009-Jedi-III/05-jedi-hands_on/main.go
+++ 009-Jedi-III/05-jedi-hands_on/main.go
@@ -4,8 +4,6 @@ import "fmt"
 
 func main() {
 	for i := 10; i <= 100; i++ {
-		if i%4 == 0 {
-			fmt.Println(i)
-		}
+		fmt.Printf("When %v is divided by 4, the remainder (aka modulus) is %v\n", i, i%4)
 	}
 }

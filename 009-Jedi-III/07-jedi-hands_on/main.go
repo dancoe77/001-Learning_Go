4bc60438f38cab3b62eec5fe9201a977ee2cf0d1
dancoe77 <dan@shoeman-tech.com>
2020-01-01 18:17:19 -0800

    commit fifty-seven

---
+++ 009-Jedi-III/07-jedi-hands_on/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 70; i <= 75; i++ {
+		if i == 70 {
+			fmt.Println("our value was 70")
+		} else if i == 71 {
+			fmt.Println("our value was 71")
+		} else if i == 72 {
+			fmt.Println("our value was 72")
+		} else if i == 73 {
+			fmt.Println("our value was 73")
+		} else if i == 74 {
+			fmt.Println("our value was 74")
+		} else {
+			fmt.Println("our value was 75")
+		}
+	}
+}

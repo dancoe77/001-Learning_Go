ede1762ece4be822c91090b96d890c0d7f28152e
dancoe77 <dan@shoeman-tech.com>
2020-01-09 14:17:40 -0800

    commit sixty-seven

--- 010-Grouping_data/02-Slice-composite_literal/main.go
+++ 010-Grouping_data/02-Slice-composite_literal/main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	// x:= type{values} // composite literal
+	// x:= type{values} // COMPOSITE LITERAL
 	x := []int{4, 5, 7, 8, 42}
 	fmt.Println(x)
 }
+
+// a SLICE allows you to group together VALUES of the same TYPE

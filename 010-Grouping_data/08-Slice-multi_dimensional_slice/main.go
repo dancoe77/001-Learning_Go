a89d702edde5a67fd37013c5cc48cc53064dbda5
dancoe77 <dan@shoeman-tech.com>
2020-01-09 23:51:49 -0800

    commit ninety-two

---
+++ 010-Grouping_data/08-Slice-multi_dimensional_slice/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	jb := []string{"James", "Bond", "Chocolate", "martini"}
+	fmt.Println(jb)
+	mp := []string{"Miss", "Moneypenny", "Strawberry", "Hazelnut"}
+	fmt.Println(mp)
+
+}

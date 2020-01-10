bf9618d60d3f85844546d2b74f236b1aa229fe89
dancoe77 <dan@shoeman-tech.com>
2020-01-09 21:49:06 -0800

    commit eighty

--- 010-Grouping_data/04-Slice-slicing_a_slice/main.go
+++ 010-Grouping_data/04-Slice-slicing_a_slice/main.go
@@ -9,4 +9,12 @@ func main() {
 	fmt.Println(x[:])
 	fmt.Println(x[1:])
 	fmt.Println(x[1:3])
+
+	for i, v := range x {
+		fmt.Println(i, v)
+	}
+
+	for i := 0; i <= 4; i++ {
+		fmt.Println(i, x[i])
+	}
 }

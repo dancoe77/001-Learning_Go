ffdb3aa0d7ebfb850ba8805f3ddd46caaec14354
dancoe77 <dan@shoeman-tech.com>
2020-02-28 21:01:50 -0800

    commit one hundred fifty

--- 012-Structs/05-Housekeeping/main.go
+++ 012-Structs/05-Housekeeping/main.go
@@ -5,17 +5,15 @@ import "fmt"
 //we create VALUES of a certain TYPE that are stored in VARIABLES
 // and those VARIABLES have IDENTIFIERS
 
-var x int
-
 type person struct {
 	first string
 	last  string
 }
 
-type foo int
-
-var y foo
-
 func main() {
-	fmt.Println("vim-go")
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+	}
+	fmt.Println(p1)
 }

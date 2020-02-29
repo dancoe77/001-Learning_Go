5a0f69d97f1b8d71d46b0011b17376f5b6e3d506
dancoe77 <dan@shoeman-tech.com>
2020-02-28 20:37:50 -0800

    commit one hundred forty-six

---
+++ 012-Structs/04-Anonymous_structs/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+	age   int
+}
+
+func main() {
+	p1 := person{
+		first: "James",
+		last:  "Bond",
+		age:   32,
+	}
+	fmt.Println(p1)
+}

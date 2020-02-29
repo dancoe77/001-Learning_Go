c4f625b5106f5e762b880737e2c5daeedd73b09b
dancoe77 <dan@shoeman-tech.com>
2020-02-28 20:44:07 -0800

    commit one hundred forty-seven

--- 012-Structs/04-Anonymous_structs/main.go
+++ 012-Structs/04-Anonymous_structs/main.go
@@ -2,14 +2,12 @@ package main
 
 import "fmt"
 
-type person struct {
-	first string
-	last  string
-	age   int
-}
-
 func main() {
-	p1 := person{
+	p1 := struct {
+		first string
+		last  string
+		age   int
+	}{
 		first: "James",
 		last:  "Bond",
 		age:   32,

6e27b24015f8e34f929aaddcd8e4010285378d53
dancoe77 <dan@shoeman-tech.com>
2020-03-05 14:23:52 -0800

    one hundred sixty

--- 014-Functions/01-Syntax/main.go
+++ 014-Functions/01-Syntax/main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	foo()
 	bar("James")
+	s1 := woo("Moneypenny")
+	fmt.Println(s1)
 }
 
 //func (r receiver) identifier(parameters) (return(s)) { ... }
@@ -13,6 +15,11 @@ func foo() {
 	fmt.Println("Hello from Foo")
 }
 
+// Everything in Go is PASS BY VALUE
 func bar(s string) {
 	fmt.Println("Hello,", s)
+}
+
+func woo(st string) string {
+	return fmt.Sprint("Hello from woo,", st)
 }

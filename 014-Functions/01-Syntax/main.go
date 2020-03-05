8e9e7e17cec6f4d608e0894628232dbe84a6c2f2
dancoe77 <dan@shoeman-tech.com>
2020-03-05 14:15:57 -0800

    one hundred fifty-nine

--- 014-Functions/01-Syntax/main.go
+++ 014-Functions/01-Syntax/main.go
@@ -4,10 +4,15 @@ import "fmt"
 
 func main() {
 	foo()
+	bar("James")
 }
 
 //func (r receiver) identifier(parameters) (return(s)) { ... }
 
 func foo() {
 	fmt.Println("Hello from Foo")
+}
+
+func bar(s string) {
+	fmt.Println("Hello,", s)
 }

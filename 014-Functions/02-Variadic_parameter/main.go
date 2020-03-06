70a97e5a2bf653f42b95056a3d4d6ce5dce063f1
dancoe77 <dan@shoeman-tech.com>
2020-03-05 19:06:11 -0800

    one hundred sixty-nine

--- 014-Functions/02-Variadic_parameter/main.go
+++ 014-Functions/02-Variadic_parameter/main.go
@@ -9,6 +9,14 @@ func main() {
 func foo(x ...int) {
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
+
+	sum := 0
+	// var sum int
+	for i, v := range x {
+		sum += v
+		fmt.Println("for item in index position,", i, "we are now adding,", v, "to the total which is now", sum)
+	}
+	fmt.Println("The total is,", sum)
 }
 
 // func (r receiver) identifier(parameter(s)) (return(s)) { code}

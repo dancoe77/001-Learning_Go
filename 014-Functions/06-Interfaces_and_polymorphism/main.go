91b69f22ea222f4380dbee3034110c579276cc7c
dancoe77 <dan@shoeman-tech.com>
2020-03-06 13:44:50 -0800

    commit one hundred eighty-seven

---
+++ 014-Functions/06-Interfaces_and_polymorphism/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+type person struct {
+	first string
+	last  string
+}
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
+// func (r receiver) identifier(parameters) (return(s)) { code}
+
+func (s secretAgent) speak() {
+	fmt.Println("I am", s.first, s.last)
+}
+
+// var x int
+// type foo int
+// a VALUE can be of more than one TYPE
+
+type human interface {
+	speak()
+}
+
+func bar(h human) {
+	fmt.Println("I called human")
+
+func main() {
+	//x := 42
+	sa1 := secretAgent{
+		person: person{
+			"James",
+			"Bond",
+		},
+		ltk: true,
+	}
+
+	sa2 := secretAgent{
+		person: person{
+			"Miss",
+			"Moneypenny",
+		},
+		ltk: true,
+	}
+
+	fmt.Println(sa1)
+	sa1.speak()
+	sa2.speak()
+}

92413be07e99e9bdfbb7796d3b604b37951aa0d8
dancoe77 <dan@shoeman-tech.com>
2020-03-06 16:44:17 -0800

    commit two hundred four

--- 014-Functions/09-Returning_a_func/main.go
+++ 014-Functions/09-Returning_a_func/main.go
@@ -5,9 +5,21 @@ import "fmt"
 func main() {
 	s1 := foo()
 	fmt.Println(s1)
+
+	x := func() int {
+		return 451
+	}
+
+	fmt.Printf("%T\n", x)
 }
 
 func foo() string {
 	s := "Hello World"
 	return s
+}
+
+func bar() func() int {
+	return func() int {
+		return 451
+	}
 }

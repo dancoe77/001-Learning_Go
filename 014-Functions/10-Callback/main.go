f7211d67ec8937e03ca4ff00760cab9a55c258bc
dancoe77 <dan@shoeman-tech.com>
2020-03-06 22:17:54 -0800

    commit two hundred thirteen

--- 014-Functions/10-Callback/main.go
+++ 014-Functions/10-Callback/main.go
@@ -19,6 +19,9 @@ func main() {
 
 	s2 := even(sum, ii...)
 	fmt.Println("Even numbers", s2)
+
+	s3 := odd(sum, ii...)
+	fmt.Println("Odd numbers", s3)
 }
 
 func sum(xi ...int) int {
@@ -34,6 +37,16 @@ func even(f func(xi ...int) int, vi ...int) int {
 	var yi []int
 	for _, v := range vi {
 		if v%2 == 0 {
+			yi = append(yi, v)
+		}
+	}
+	return f(yi...)
+}
+
+func odd(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 != 0 {
 			yi = append(yi, v)
 		}
 	}

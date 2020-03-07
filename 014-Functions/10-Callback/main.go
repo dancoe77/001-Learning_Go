deb02683fd1783df60164478e19cbef3c2461f52
dancoe77 <dan@shoeman-tech.com>
2020-03-06 21:23:47 -0800

    commit two hundred eleven

---
+++ 014-Functions/10-Callback/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	// x := 0
+	// fmt.Println(x)
+	// x++
+	// fmt.Println(x)
+	// x += 42
+	// fmt.Println(x)
+	// x-= 3
+	// fmt.Println(x)
+	// x--
+	// fmt.Println(x)
+	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	s := sum(ii...)
+	fmt.Println("All numbers", s)
+
+	s2 := even(sum, ii...)
+	fmt.Println("Even numbers", s2)
+}
+
+func sum(xi ...int) int {
+	fmt.Printf("%T\n", xi)
+	total := 0
+	for _, v := range xi {
+		total += v
+	}
+	return total
+}
+
+func even(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 == 0 {
+			yi = append(yi, v)
+		}
+	}
+	return f(yi...)
+}

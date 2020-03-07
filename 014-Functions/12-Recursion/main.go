4013ac661c3c196e3c00cb39bc34777426ea5412
dancoe77 <dan@shoeman-tech.com>
2020-03-06 23:40:34 -0800

    commit two hundred twenty-five

--- 014-Functions/12-Recursion/main.go
+++ 014-Functions/12-Recursion/main.go
@@ -5,4 +5,26 @@ import "fmt"
 func main() {
 	//Four factorial
 	fmt.Println(4 * 3 * 2 * 1)
+	n := factorial(4)
+	fmt.Println(n)
+
+	// n2 := loopFact(4)
+	// fmt.Println(n2)
 }
+
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+//4 * 3 * 2 * 1
+
+//func loopFact(n int) int {
+//	total := 1
+//	for ; n > 0; n-- {
+//		total *= n
+//	}
+//	return total
+//}

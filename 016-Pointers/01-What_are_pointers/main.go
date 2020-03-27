82d97c052a876c65d53461a95762abe4f95347ca
dancoe77 <dan@shoeman-tech.com>
2020-03-26 17:50:52 -0700

    commit two hundred forty-three

--- 016-Pointers/01-What_are_pointers/main.go
+++ 016-Pointers/01-What_are_pointers/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	a := 42
 	fmt.Println(a)
-	fmt.Println(&a)
+	fmt.Println(&a) // & gives you the address
 
 	fmt.Printf("%T\n", a)
 	fmt.Printf("%T\n", &a)
@@ -13,7 +13,10 @@ func main() {
 	//var b int = &a {can't use *int as int}
 	var b *int = &a
 	fmt.Println(b)
-	fmt.Println(*b)
+	fmt.Println(*b) // * gives me the value stored at an address
 	fmt.Println(*&a)
 	//b := &a
+
+	*b = 43
+	fmt.Println(a)
 }

478166ece35c08b13f3242813ed7999de234810a
dancoe77 <dan@shoeman-tech.com>
2020-03-26 18:13:43 -0700

    commit two hundred forty-seven

--- 016-Pointers/02-When_to_use_pointers/main.go
+++ 016-Pointers/02-When_to_use_pointers/main.go
@@ -4,14 +4,17 @@ import "fmt"
 
 func main() {
 	x := 0
+	fmt.Println("x before", &x)
+	fmt.Println("x before", x)
 	foo(&x)
-	fmt.Println(x)
+	fmt.Println("x after", &x)
+	fmt.Println("x after", x)
 }
 
 func foo(y *int) {
-	fmt.Println(y)
-	fmt.Println(*y)
+	fmt.Println("y before", y)
+	fmt.Println("y before", *y)
 	*y = 43
-	fmt.Println(y)
-	fmt.Println(*y)
+	fmt.Println("y after", y)
+	fmt.Println("y after", *y)
 }

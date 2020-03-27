2fb9c98da353731904dfe5529f50a05825886c98
dancoe77 <dan@shoeman-tech.com>
2020-03-26 17:38:59 -0700

    commit two hundred forty

--- 016-Pointers/01-What_are_pointers/main.go
+++ 016-Pointers/01-What_are_pointers/main.go
@@ -6,4 +6,11 @@ func main() {
 	a := 42
 	fmt.Println(a)
 	fmt.Println(&a)
+
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", &a)
+
+	//var b int = &a {can't use *int as int}
+	var b *int = &a
+	fmt.Println(b)
 }
